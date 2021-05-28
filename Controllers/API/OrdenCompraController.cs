55cc70c066f28da7f825743253c56212856f074f
josezt1992 <josezt23@gmail.com>
2021-05-27 23:25:38 -0500

    Generar Orden de Compra

---
+++ Controllers/API/OrdenCompraController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SGOALB_BACK.Controllers.API
+{
+    public class OrdenCompraController : ApiController
+    {
+    }
+}
