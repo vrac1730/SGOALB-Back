61b2bff64aba24eb6762e7ecef3bdef4d1cd5453
Arnold <Arnold.alegriap@gmail.com>
2021-06-25 20:15:09 -0500

    crud producto proveedor local

---
+++ Controllers/LocalController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using SGOALB_BACK.Models;
+using System.Data;
+using System.Net;
+
+namespace SGOALB_BACK.Controllers
+{
+    public class LocalController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Locals
+        public ActionResult Index()
+        {
+            return View(db.Locales.ToList());
+        }
+
+        // GET: Locals/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Local local = db.Locales.Find(id);
+            if (local == null)
+            {
+                return HttpNotFound();
+            }
+            return View(local);
+        }
+
+        // GET: Locals/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Locals/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nombre,direccion,telefono,ruc,razon_social")] Local local)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Locales.Add(local);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(local);
+        }
+
+        // GET: Locals/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Local local = db.Locales.Find(id);
+            if (local == null)
+            {
+                return HttpNotFound();
+            }
+            return View(local);
+        }
+
+        // POST: Locals/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nombre,direccion,telefono,ruc,razon_social")] Local local)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(local).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(local);
+        }
+
+        // GET: Locals/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Local local = db.Locales.Find(id);
+            if (local == null)
+            {
+                return HttpNotFound();
+            }
+            return View(local);
+        }
+
+        // POST: Locals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Local local = db.Locales.Find(id);
+            db.Locales.Remove(local);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/ProductoController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SGOALB_BACK.Models;
+
+namespace SGOALB_BACK.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Producto
+        public ActionResult Index(string cadena, string cod)
+
+        {
+            //validar simbolos y caracteres no permitidos
+            //mostrar otra vista para resultados no encontrados?
+            if (String.IsNullOrWhiteSpace(cadena) && String.IsNullOrWhiteSpace(cod))
+            {
+                var result = db.Productos.Include(p => p.Categoria).Include(p=>p.Alerta).ToList();
+                return View(result);
+            }
+
+            else if (cod.Length > 0)
+            {
+                var r1 = db.Productos.Include(p => p.Categoria).Include(p => p.Alerta).Where(p => p.codigo == cod).ToList();
+                return View(r1);
+            }
+
+            else if (cadena.Length > 0)
+            {
+                var r2 = db.Productos.Include(p => p.Categoria).Include(p => p.Alerta).Where(p => p.nombre.Contains(cadena)).ToList();
+                return View(r2);
+            }
+
+            var resultado = db.Productos.Include(p => p.Categoria).Include(p => p.Alerta).ToList();
+            return View(resultado);
+
+        }
+
+        // GET: Producto/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Productos.Include(p => p.Categoria).Include(p => p.Alerta).FirstOrDefault(p=>p.id==id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        // GET: Producto/Create
+        public ActionResult Create()
+        {
+            ViewBag.idAlerta = new SelectList(db.Alertas, "id", "nombre");
+            ViewBag.idCategoria = new SelectList(db.Categorias, "id", "nombre");
+            return View();
+        }
+
+        // POST: Producto/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,codigo,nombre,descripcion,marca,stock_min,stock_max,idCategoria,idAlerta")] Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Productos.Add(producto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idAlerta = new SelectList(db.Alertas, "id", "nombre", producto.idAlerta);
+            ViewBag.idCategoria = new SelectList(db.Categorias, "id", "nombre", producto.idCategoria);
+            return View(producto);
+        }
+
+        // GET: Producto/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Productos.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idAlerta = new SelectList(db.Alertas, "id", "nombre", producto.idAlerta);
+            ViewBag.idCategoria = new SelectList(db.Categorias, "id", "nombre", producto.idCategoria);
+            return View(producto);
+        }
+
+        // POST: Producto/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,codigo,nombre,descripcion,marca,stock_min,stock_max,idCategoria,idAlerta")] Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(producto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idAlerta = new SelectList(db.Alertas, "id", "nombre", producto.idAlerta);
+            ViewBag.idCategoria = new SelectList(db.Categorias, "id", "nombre", producto.idCategoria);
+            return View(producto);
+        }
+
+        // GET: Producto/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Productos.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        // POST: Producto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Producto producto = db.Productos.Find(id);
+            db.Productos.Remove(producto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
